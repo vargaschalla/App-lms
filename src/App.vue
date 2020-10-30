c182a3c05f2e153555c398fe4007314f1b9a4627
wagner <wagner.vargas@upeu.edu.pe>
2020-10-30 10:10:18 -0500

    VueV1

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="appx">
+    <p>WELCOME</p>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome" msg2="Hola "/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+.appx {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.appx p{
+  color: #006356;
+  font-family: times new roman, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p> 
+      {{ msg2 }}
+      {{ message }}
+  <input v-model="message">
+
+
+    </p>
+
+    <button
+      type="button"
+      class="btn btn-warning btn-sm"
+      @click="editBook(msg)">
+      Update
+    </button>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: function() {
+    return {
+      message: "Wagner", num2: ""
+    }
+  },   
+  props: {
+    msg: String,
+    msg2: String,
+    
+  },
+  methods: {
+    getBooks() {
+      const path = 'Hola has create';
+      console.log(path);
+        
+    },
+    addBook(payload) {
+      const path = 'http://localhost:5001/books';
+      console.log(path+payload);
+    },
+    editBook(cad) {
+      console.log("edit "+cad+": "+this.message);
+      
+    },
+  },
+  created() {
+    this.getBooks();
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.hello {
+color: #1cca9f;
+font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+}
+</style>
